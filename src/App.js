641fde723aa4088648c6badb41c286636457130d
hariomvc <chaudharihariomvijay@gmail.com>
2022-05-02 17:58:35 +0530

    Added & Tested React router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import Catalogue from "./Components/Catalogue/Catalogue";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
+
+function Test(){
+  let { id } = useParams();
+
+  return (
+    <div>
+      <h3>ID: {id}</h3>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      <Catalogue/>
+      <Router>
+      <Switch>
+        <Route exact path="/">
+        <Catalogue/>
+          </Route>
+          <Route path="/:id">
+            <Test/>
+            </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
